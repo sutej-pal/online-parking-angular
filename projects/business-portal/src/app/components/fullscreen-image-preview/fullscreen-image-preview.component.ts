3b646d21089505bed789ce50ba6a2ebb6dbfa120
Sutej Pal <sutejpal@hotmail.com>
2022-03-03 20:44:35 +0530

    business portal changes

--- projects/business-portal/src/app/components/fullscreen-image-preview/fullscreen-image-preview.component.ts
+++ projects/business-portal/src/app/components/fullscreen-image-preview/fullscreen-image-preview.component.ts
@@ -1,6 +1,10 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 
+interface ImageData {
+  src: string
+}
+
 @Component({
   selector: 'app-fullscreen-image-preview',
   templateUrl: './fullscreen-image-preview.component.html',
@@ -10,7 +14,7 @@ export class FullscreenImagePreviewComponent implements OnInit {
 
   constructor(
     public dialogRef: MatDialogRef<FullscreenImagePreviewComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: ImageData
   ) {
   }
 
