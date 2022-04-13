26d4d8d186e60dc28b8214de5ff3c35673c76f26
sutej-pal <sutejpal@hotmail.com>
2022-04-13 22:32:23 +0530

    Client portal changes

--- projects/client-portal/src/app/store/individual/individual.actions.spec.ts
+++ projects/client-portal/src/app/store/individual/individual.actions.spec.ts
@@ -2,6 +2,6 @@ import * as fromIndividual from './individual.actions';
 
 describe('loadIndividuals', () => {
   it('should return an action', () => {
-    expect(fromIndividual.loadIndividuals().type).toBe('[Individual] Load Individuals');
+    expect(fromIndividual.loadIndividual().type).toBe('[Individual] Load Individuals');
   });
 });
