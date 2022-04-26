cba707aef4af3981f01973ad3f8bd2a0d54edc22
sutej-pal <sutejpal@hotmail.com>
2022-04-26 16:51:55 +0530

    Client portal changes

--- projects/client-portal/src/app/screens/search/search.component.ts
+++ projects/client-portal/src/app/screens/search/search.component.ts
@@ -3,13 +3,14 @@ import {AppComponent} from "../../app.component";
 import {google} from 'google-maps';
 import {Store} from "@ngrx/store";
 import {getSearchData} from "../../store/search/search.selectors";
-import {updateSearch} from "../../store/search/search.actions";
 import {Observable} from "rxjs";
 import {searchData} from "../../store/search/search.reducer";
 import {MatDrawer} from "@angular/material/sidenav";
 import {ParkingLot} from "../../types/types";
 import {HttpService} from "../../../../../common-services/http.service";
 import {Router} from "@angular/router";
+import * as _ from "underscore";
+import {NotificationService} from "../../../../../common-services/notification.service";
 
 @Component({
   selector: 'app-search',
@@ -33,27 +34,31 @@ export class SearchComponent implements OnInit, OnDestroy {
   searchData$: Observable<searchData> | undefined;
   selectedParkingLot: ParkingLot | undefined;
   isPLDetailWindowExpanded = false;
+  googleMap: google.maps.Map | undefined;
+  mapMarkers: google.maps.Marker[] | undefined;
 
   constructor(
     private store: Store,
     private router: Router,
     private httpService: HttpService,
+    private notificationService: NotificationService
   ) {
   }
 
   async ngOnInit() {
     this.searchData$ = this.store.select(getSearchData);
     await this.checkIfMapLoaded();
-    this.test();
-    this.searchData$.subscribe(async e => {
-      await this.getParkingLots(e);
-    })
+    this.updateGoogleMapAutocompletePosition();
   }
 
   async checkIfMapLoaded() {
     this.google = await AppComponent.googleMap;
     if (this.google && this.google.maps) {
       await this.loadGoogleMap();
+      this.searchData$?.subscribe(async e => {
+        await this.getParkingLots(e);
+        this.addMarkerOnMap();
+      })
     } else {
       setTimeout(async () => {
         await this.checkIfMapLoaded();
@@ -65,9 +70,10 @@ export class SearchComponent implements OnInit, OnDestroy {
     const google = await AppComponent.googleMap
     const MapTypeId = google.maps.MapTypeId;
     this.mapOptions['mapTypeId'] = MapTypeId.ROADMAP;
-    const map = new google.maps.Map(this.map?.nativeElement, this.mapOptions);
+    this.googleMap = new google.maps.Map(this.map?.nativeElement, this.mapOptions);
     this.searchData$?.subscribe(e => {
-      map.setCenter({lat: e.lat, lng: e.lng});
+      this.googleMap?.setCenter({lat: e.lat, lng: e.lng});
+      this.addMarkerOnMap();
     })
   }
 
@@ -79,7 +85,7 @@ export class SearchComponent implements OnInit, OnDestroy {
     }
   }
 
-  test() {
+  updateGoogleMapAutocompletePosition() {
     const style = document.createElement('style');
     style.type = 'text/css';
     style.id = 'google-autocomplete'
@@ -101,10 +107,56 @@ export class SearchComponent implements OnInit, OnDestroy {
   async getParkingLots(data: searchData) {
     try {
       const result = await this.httpService.executeRequest('search', 'post', data).toPromise();
-      console.log('Success', result);
       this.parkingLotList = result.body.data;
     } catch (e) {
       console.log('Failed');
+      this.notificationService.showError('Failed to load Parking Lots.');
     }
+  }
+
+  sortParkingLotsByPrice() {
+    this.parkingLotList = _.sortBy(this.parkingLotList, (parkingLot) => {
+      return parkingLot?.parkingSpot?.price
+    });
+  }
+
+  sortParkingLotsByDistance() {
+
+  }
+
+  addMarkerOnMap() {
+    let map = this.googleMap;
+
+    // Deletes all markers in the array by removing references to them.
+    const deleteMarkers = () => {
+      setMapOnAll(null);
+      this.mapMarkers = [];
+    }
+
+    // Sets the map on all this.mapMarkers in the array.
+    const setMapOnAll = (map: google.maps.Map<Element> | google.maps.StreetViewPanorama | null) => {
+      const length = this.mapMarkers?.length || 0
+      for (let i = 0; i < length; i++) {
+        // @ts-ignore
+        this.mapMarkers[i].setMap(map);
+      }
+    }
+
+    const addMarker = (location: { lat: number; lng: number } | undefined) => {
+      const marker = new google.maps.Marker({
+        position: location,
+        map: map,
+        icon: '../../../assets/images/parking.png'
+      });
+      // @ts-ignore
+      this.mapMarkers.push(marker);
+    }
+
+    deleteMarkers();
+
+
+    this.parkingLotList.map(parkingLot => {
+      addMarker(parkingLot?.geometry);
+    })
   }
 }
--- projects/client-portal/src/app/types/types.ts
+++ projects/client-portal/src/app/types/types.ts
@@ -1,5 +1,12 @@
 export interface ParkingLot {
   name: string;
+  parkingSpot?: {
+    price: number
+  };
+  geometry: {
+    lat: number,
+    lng: number,
+  },
   amenities: {
     cctv: boolean,
     secured: boolean,
